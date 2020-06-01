f7feb04029a311d8004001d8771571d2df517f82
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-06-01 12:43:09 -0400

    Created historical search list

--- script.js
+++ script.js
@@ -1,46 +1,55 @@
 const key = '2d16edaf814c74f9e0898876fc6cc68f'
 const search = 'Toronto'
 let unit = 'metric'
-let country;
 let geolocation = localStorage.geolocation;
+let searchlist = (localStorage.searchlist)? JSON.parse(localStorage.searchlist) : [];
 
 startup()
 
+//At Start
 function startup(){
-    loadData(search,unit,);
-    if (geolocation) loadData(search,unit,JSON.parse(geolocation));
+    loadData(search,unit,null,false);
+    if (geolocation) loadData(search,unit,JSON.parse(geolocation),false);
     else geoLocation();
 }
 
-
+//Get Geo Location
 function geoLocation(){
     navigator.geolocation.getCurrentPosition(async function(position){ 
-        loadData(search,unit,position.coords);
+        loadData(search,unit,position.coords,false);
         localStorage.geolocation = JSON.stringify({latitude:position.coords.latitude,longitude:position.coords.longitude})
     });
 }
 
-
-async function loadData(search,unit,geo){
+//Fetch and Load Data
+async function loadData(search,unit,geo,isSearch){
     const parameters = (geo != null)? `lat=${geo.latitude}&lon=${geo.longitude}`: `q=${search}`;
     const currentFetch = await fetch(`https://api.openweathermap.org/data/2.5/weather?${parameters}&units=${unit}&appid=${key}`)
-
+    
     currentFetch.text().then(function(data){
         const getData = JSON.parse(data)
         const icon = `http://openweathermap.org/img/wn/${getData.weather[0].icon}.png`
-        country = getData.sys.country
+        const getCity = `${getData.name}, ${getData.sys.country}`
+        
+        if (isSearch){
+            if (searchlist.includes(getCity)) searchlist.splice(searchlist.indexOf(getCity),1);
+            searchlist.push(getCity);
+            localStorage.searchlist = JSON.stringify(searchlist)
+        } 
+        
 
         document.querySelector('#name').innerHTML = `<span id='city'>${getData.name}</span> (${moment(getData.dt,'X').format('L')}) <img src='${icon}'>`;
         document.querySelector('#temp').innerHTML = `Temperature: ${getData.main.temp.toFixed(1)}\xB0${(unit=='metric')? 'C': 'F'}` ;
         document.querySelector('#humidity').innerHTML = `Humidity: ${getData.main.humidity}%`
         document.querySelector('#windSpeed').innerHTML = `Wind Speed: ${getData.wind.speed} ${(unit=='metric')? 'm/s': 'mph'}`
        
-        
         getForecast(getData.coord.lat,getData.coord.lon)
+        searchList()
     })
 }
 
 
+//Get Forecast
 async function getForecast(lat,lon){
     const forecastFetch = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${unit}&exclude=minutely,hourly&appid=${key}`);
 
@@ -69,6 +78,7 @@ async function getForecast(lat,lon){
 }
 
 
+//Change Unit Metric/Imperial
 function changeUnit(){
     unit = event.target.id
     loadData(document.querySelector('#city').textContent,unit,)
@@ -79,9 +89,24 @@ function changeUnit(){
     })
 }
 
+//At Search Button
 function searchCity() {
     event.preventDefault();
     const searchCity = document.querySelector('#search');
-    loadData(searchCity.value,unit,);
+    loadData(searchCity.value,unit,null,true);
     searchCity.value = '';
+}
+
+//Historical Search List
+function searchList() {
+    const searchListHTML = document.querySelector('#searchListID')
+    searchListHTML.innerHTML =''
+    for (const city of searchlist){
+        searchListHTML.innerHTML = `<li class="list-group-item"><a href="#" onclick="onClickList()" >${city}</a></li> ` + searchListHTML.innerHTML ;
+    }
+}
+
+//At Historical Search List
+function onClickList(){
+    loadData(event.target.textContent,unit,null,true)
 }
